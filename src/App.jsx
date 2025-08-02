97e72440d89656fc076ec0fb9c3df38129d87741
JoaoMeisterI <joaopedromeisterivacow@gmail.com>
2025-08-02 02:08:23 -0300

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import Tasks from "./Tasks";
+
+function App(){
+  return (
+    <div>
+      <h1>TESTE</h1>
+      <Tasks/>
+    </div>
+  )
+}
+
+export default App;
---
+++ src/components/Tasks.jsx
@@ -0,0 +1,7 @@
+function Tasks(){
+    return(
+        <h1>aqui deu boa</h1>
+    )
+}
+
+export default Tasks
